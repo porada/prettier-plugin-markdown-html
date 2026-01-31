a373c50f5e074bcc6d5b35038897f19141d8111f
Dom Porada <dom@dom.engineering>
2026-01-31 15:54:04 +0100

    Update dependencies

--- prettier.config.ts
+++ prettier.config.ts
@@ -1,16 +1,8 @@
 import { defineConfig } from '@standard-config/prettier';
 import * as pluginMarkdownHTML from './src/index.ts';
 
-const config = defineConfig();
-
-config.plugins!.push(pluginMarkdownHTML);
-
-config.overrides!.push({
-	files: ['*.md'],
-	options: {
-		printWidth: Number.POSITIVE_INFINITY,
-		singleAttributePerLine: true,
+export default defineConfig({
+	pluginOverrides: {
+		'prettier-plugin-markdown-html': pluginMarkdownHTML,
 	},
 });
-
-export default config;
