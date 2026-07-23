5a86039606063c294d655da3ea91194ab09c521b
Dom Porada <dom@dom.engineering>
2026-07-23 19:15:38 +0200

    Update dependencies

--- prettier.config.ts
+++ prettier.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from '@standard-config/prettier';
 import * as pluginMarkdownHTML from './src/index.ts';
 
 export default defineConfig({
+	/* oxlint-disable-next-line typescript/no-deprecated */
 	pluginOverrides: {
 		'prettier-plugin-markdown-html': pluginMarkdownHTML,
 	},
